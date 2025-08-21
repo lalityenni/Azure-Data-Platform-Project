d4a165e167e947c56b9b0a8f390cc32323f882c2
lalityenni <lalit.yenni@gmail.com>
2025-08-21 16:44:12 -0700

    chore: add terraform.required_version and provider versions (tflint fix)

---
+++ infra/m1-network/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.116"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name = "rg-adp-dev-eus"
+  location = "East US"
+}
