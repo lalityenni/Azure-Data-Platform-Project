cbf2c25c260d88571d1d96abf2ab50932380f652
lalityenni <lalit.yenni@gmail.com>
2025-08-28 17:42:55 -0700

    M1.2: add hub subnet

--- infra/m1-network/main.tf
+++ infra/m1-network/main.tf
@@ -30,3 +30,10 @@ resource "azurerm_virtual_network" "hub" {
     tier = "network"
   }
 }
+
+resource "azurerm_subnet" "hub_subnet" {
+  name                 = "snet-adp-dev-eus-hub"           # shows in Azure
+  resource_group_name  = azurerm_resource_group.rg.name   # ties to your RG
+  virtual_network_name = azurerm_virtual_network.hub.name # inside your VNet
+  address_prefixes     = ["10.0.1.0/24"]                  # leaves 10.0.0.x free
+}
