e633991aed21a02e0bdcad3671c671f416d5dfef
lalityenni <lalit.yenni@gmail.com>
2025-09-03 16:58:37 -0700

    added variables.tf

---
+++ infra/m1-network/variables.tf
@@ -0,0 +1,8 @@
+variable "rg_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+variable "location" {
+  description = "The Azure region to deploy resources in"
+  type        = string
+}
