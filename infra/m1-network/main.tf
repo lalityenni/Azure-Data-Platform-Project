69d228d8934bcedb76c5eff0c8bc63575f3dfe8a
lalityenni <lalit.yenni@gmail.com>
2025-09-03 12:10:19 -0700

    M2.2: add storage Private Endpoint (blob)

--- infra/m1-network/main.tf
+++ infra/m1-network/main.tf
@@ -127,3 +127,23 @@ resource "azurerm_storage_container" "staging" {
   container_access_type = "private"
 
 }
+
+resource "azurerm_private_endpoint" "st_blob_pe" {
+  name                = "pep-adp-dev-eus-st-blob"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.data_pe.id
+
+  private_service_connection {
+    name                           = "psc-st-blob"
+    private_connection_resource_id = azurerm_storage_account.landing.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"] # target the blob endpoint of the storage account
+  }
+
+  tags = {
+    env  = "dev"
+    tier = "network"
+  }
+
+}
